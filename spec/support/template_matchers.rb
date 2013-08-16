9594b2555473ea98e135031c9ff5c99ddf7349a3
Nick Clark <nick.clark@k3integrations.com>
2013-08-16 11:58:58 -0600

    Add with_template_id and and_content fluency to define_module matcher

--- spec/support/template_matchers.rb
+++ spec/support/template_matchers.rb
@@ -10,21 +10,61 @@ end
 RSpec::Matchers.define :define_module do |module_name|
 
   match do |template|
-    modules_from(template).include? module_name
+    @modules = modules_from(template)
+    @mod = @modules[module_name]
+    @content = @mod.templates[@template_id]
+    has_module && has_template_id && has_content
+  end
+
+  chain :with_template_id do |template_id|
+    @template_id = template_id
+  end
+
+  chain :and_content do |content|
+    @expected_content = content
+  end
+
+  def has_module
+    !!@mod
+  end
+
+  def has_template_id
+    return true unless @template_id
+    @template_id && @content
+  end
+
+  def has_content
+    return true unless @expected_content
+    @expected_content && (@content == @expected_content)
   end
 
   failure_message_for_should do |template|
-    "Template expected to define module '#{module_name}', found: #{modules_from(template)}"
+    failure_message
+  end
+
+  def failure_message(not_msg: nil)
+    msg = "Template expected"
+    msg += ' not' if not_msg
+
+    msg + if !has_module
+      " to define module '#{module_name}', found: #{@modules.keys}"
+    elsif !has_template_id
+      " to define template_id '#{@template_id}', found: #{@mod.templates.keys}"
+    elsif !has_content
+      " to have content #{@expected_content.inspect}, found: #{@content.inspect}"
+    else
+      "; Unknown reason"
+    end
   end
 
   failure_message_for_should_not do |template|
-    "Template not expected to define module '#{module_name}', found: #{modules_from(template)}"
+    failure_message('not')
   end
 
   def modules_from(template)
     cxt = V8::Context.new
     cxt.eval(Angular::Html2js::TEMPLATE_HELPER, "spec/helpers/template_cache.coffee")
     cxt['template'] = template
-    cxt.eval('evaluateTemplate(template)').keys
+    cxt.eval('evaluateTemplate(template)')
   end
 end
