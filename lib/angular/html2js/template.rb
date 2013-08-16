938f5769bd6d30a9c1b12b228f08828dd70d8899
Nick Clark <nick.clark@k3integrations.com>
2013-08-16 12:00:14 -0600

    Initial tilt template with support for converting code and preserving new lines.

---
+++ lib/angular/html2js/template.rb
@@ -0,0 +1,55 @@
+require 'tilt'
+
+module Angular
+  module Html2js
+    class Template < Tilt::Template
+      attr_accessor :file
+
+      TEMPLATE = <<-TEMPLATE
+angular.module(\'%s\', []).run(function($templateCache) {
+  $templateCache.put(\'%s\',
+  \'%s\');
+});
+      TEMPLATE
+
+
+      SINGLE_MODULE_TPL = <<-SINGLE_MODULE_TPL
+(function(module) {
+  try {
+    module = angular.module(\'%s\');
+  } catch (e) {
+    module = angular.module(\'%s\', []);
+  }
+  module.run(function($templateCache) {
+    $templateCache.put(\'%s\',
+    \'%s\');
+  });
+})();
+      SINGLE_MODULE_TPL
+
+      def module_name
+        nil
+      end
+
+      def prepare
+      end
+
+      def evaluate(scope, locals, &block)
+        if module_name
+          SINGLE_MODULE_TPL % [moduleName, moduleName, htmlPath, escapeContent(content)]
+        else
+          TEMPLATE % [file, file, escapeContent(data)]
+        end
+      end
+
+      private
+
+      def escapeContent(content)
+        content.gsub(/\n/, "\\\\n\' +\n   \'")
+      end
+
+    end
+  end
+end
+
+Tilt.register Angular::Html2js::Template, 'ng'
---
+++ spec/angular/html2js/template_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+require 'angular/html2js/template'
+
+module Angular
+  module Html2js
+    describe Template do
+
+
+      it 'should convert html to js code' do
+        result = process '<h1>hello</h1>', 'tpl.html'
+        result.should define_module('tpl.html').
+          with_template_id('tpl.html').
+          and_content('<h1>hello</h1>')
+      end
+
+      it 'should preserve new lines' do
+        result = process "first\nsecond", 'path/tpl.html'
+        result.should define_module('path/tpl.html').
+          with_template_id('path/tpl.html').
+          and_content("first\nsecond")
+      end
+
+      def process(template_str, file_name, locals={})
+        template = Tilt['ng'].new { template_str }
+        template.file = file_name
+        template.render(self, locals)
+      end
+
+    end
+  end
+end
