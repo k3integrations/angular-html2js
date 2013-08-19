78959a5cd93dcdf05d28207a2eff51b9f90618ac
Nick Clark <nick.clark@k3integrations.com>
2013-08-19 12:29:20 -0600

    Move config init to before_configuration hook in Rails

--- lib/angular/html2js/railtie.rb
+++ lib/angular/html2js/railtie.rb
@@ -6,8 +6,11 @@ module Angular
     class Railtie < Rails::Railtie
       railtie_name :angular_html2js
 
-      initializer "angular_html2js.configure_rails_initialization" do
+      config.before_configuration do
         config.angular_html2js = Angular::Html2js.config
+      end
+
+      initializer "angular_html2js.configure_rails_initialization" do
         Sprockets.register_engine '.ngt', Angular::Html2js::Engine
       end
     end
